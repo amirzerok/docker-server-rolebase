703036d4e89b8274e416fd61d035b4e91e94afc3
Amirali Hashemipoor <113814815+amirzerok@users.noreply.github.com>
2024-11-28 06:07:26 +0330

    Add files via upload

--- Student-attendance-system-next-js-main/pages/api/users/[id].ts
+++ Student-attendance-system-next-js-main/pages/api/users/[id].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios, { AxiosError } from 'axios';
 
-const API_URL = 'http://localhost:3001/users';
+const API_URL = 'http://nestjs:3001/users';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
